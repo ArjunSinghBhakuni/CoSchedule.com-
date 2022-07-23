82b3c31ea448219fe2894b5329f9fbc7f592c3aa
ArjunSinghBhakuni <101566727+ArjunSinghBhakuni@users.noreply.github.com>
2022-07-23 16:05:51 +0530

    Project-completed 80%

---
+++ src/components/calender/DayProjects.jsx
@@ -0,0 +1,23 @@
+import React,{useContext} from 'react'
+import {Box,Text,Avatar} from "@chakra-ui/react"
+import { DayContext } from '../../context/DayContext'
+import { useNavigate} from "react-router-dom"
+import "./day.css";
+
+const DayProjects = ({title,refNO}) => {
+ const { setNewproject, setProjectrefNo } = useContext(DayContext);
+const navigate = useNavigate()
+ const handleClick = (refNO)=>{
+  setProjectrefNo(refNO);
+  navigate("/editproject");
+ }
+
+  return (
+    <Box border="1px solid grey" h="30px" onClick={()=>handleClick(refNO)} className="pro">
+     <Text>{title}</Text>
+     <Avatar size={"sm"} src='https://bit.ly/sage-adebayo' />
+    </Box>
+  )
+}
+
+export default DayProjects
