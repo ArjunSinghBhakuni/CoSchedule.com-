4edd93992e53942cfd360f5c6cdc20c6ae209835
ArjunSinghBhakuni <101566727+ArjunSinghBhakuni@users.noreply.github.com>
2022-07-23 20:25:59 +0530

    Delete src directory

--- src/Redux/App_reducer/actiontypes.js
+++
@@ -1,6 +0,0 @@
- 
-export const ADD_NEW_PROJECT  ="ADD_NEW_PROJECT "
-
-export const Delete_PROJECT  ="Delete_PROJECT "
-
-export const EDIT_PROJECT  ="EDIT_PROJECT"
--- src/Redux/logger/action.js
+++
@@ -1,5 +0,0 @@
-// action type
-export const IS_LOGIN = "IS_LOGIN";
-
-// action-creator
-export const isLogin = (obj) => ({ type: IS_LOGIN, payload: obj });
